481c267ca0f78c3975390c85283b1fba5bb1e77d
Emery dela Cruz <emery.delacruz@gmail.com>
2017-05-19 20:41:49 +0800

    Added custom PersonProvider for authenticator fix

---
+++ src/AppBundle/Security/PersonProvider.php
@@ -0,0 +1,44 @@
+<?php
+namespace AppBundle\Security;
+
+use AppBundle\Entity\Person;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+
+class PersonProvider implements UserProviderInterface
+{
+    public function loadUserByUsername($username)
+    {
+        $userData = ...
+        // pretend it returns an array on success, false if there is no user
+
+        if ($userData) {
+            $password = '...';
+
+
+            return new Person($username, $password, $roles);
+        }
+
+        throw new UsernameNotFoundException(
+            sprintf('Username "%s" does not exist.', $username)
+        );
+    }
+
+    public function refreshUser(UserInterface $user)
+    {
+        if (!$user instanceof WebserviceUser) {
+            throw new UnsupportedUserException(
+                sprintf('Instances of "%s" are not supported.', get_class($user))
+            );
+        }
+
+        return $this->loadUserByUsername($user->getUsername());
+    }
+
+    public function supportsClass($class)
+    {
+        return Person::class === $class;
+    }
+}
